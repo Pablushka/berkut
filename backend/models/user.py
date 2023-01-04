135417b47a8b381055a25042827beffa505b056e
Pablushka <pablushka@nomades.com.ar>
2023-01-03 21:33:56 -0300

    Create new model for users

---
+++ backend/models/user.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from datetime import datetime
+from database import db
+
+
+@dataclass
+class User(db.Model):  # type: ignore
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    last_login = db.Column(db.DateTime, nullable=True,
+                           default=datetime.utcnow)
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f"User('{self.id}', '{self.name}', '{self.email}', '{self.last_login}', '{self.is_active}')"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'last_login': self.last_login.strftime("%d-%m-%Y"),
+            'is_active': self.is_active
+        }
