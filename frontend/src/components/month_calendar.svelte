58c01aded67775bb5469432353145f1df78408f9
Pablushka <pablushka@nomades.com.ar>
2023-02-14 21:03:26 -0300

    add calendar component

--- frontend/src/components/month_calendar.svelte
+++ frontend/src/components/month_calendar.svelte
@@ -1,20 +1,64 @@
 <script>
-
   export let month = "Enero";
   export let onClick = null;
-
 </script>
 
-<div class="card" on:click={onClick}>{month}</div>
+<div class="mes">
+  <h2 class="h5 text-center">{month}</h2>
+  <table class="table table-striped text-center" role="presentation">
+    <tbody>
+      <tr>
+        <td aria-hidden="true" class="text-center dias">Dom</td>
+        <td aria-hidden="true" class="text-center dias">Lun</td>
+        <td aria-hidden="true" class="text-center dias">Mar</td>
+        <td aria-hidden="true" class="text-center dias">Mié</td><td
+          aria-hidden="true"
+          class="text-center dias">Jue</td
+        ><td aria-hidden="true" class="text-center dias">Vie</td><td
+          aria-hidden="true"
+          class="text-center dias">Sáb</td
+        ></tr
+      ><tr
+        ><td class="today bg-primary">1</td><td>2</td><td>3</td><td>4</td><td
+          >5</td
+        ><td>6</td><td>7</td></tr
+      ><tr
+        ><td>8</td><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td
+          >14</td
+        ></tr
+      ><tr
+        ><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td
+          >21</td
+        ></tr
+      ><tr
+        ><td>22</td><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td
+          >28</td
+        ></tr
+      ><tr
+        ><td>29</td><td>30</td><td>31</td><td>&nbsp;</td><td>&nbsp;</td><td
+          >&nbsp;</td
+        ><td>&nbsp;</td></tr
+      ><tr
+        ><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td
+          >&nbsp;</td
+        ><td>&nbsp;</td><td>&nbsp;</td></tr
+      ></tbody
+    >
+  </table>
+</div>
 
 <style>
+  .mes {
+    background-color: #e7e7e5a3;
+    height: 340px;
+    margin: 10px;
+  }
   .card {
     width: 100px;
     height: 100px;
     background-color: rgb(233, 222, 222);
     margin: 10px;
     flex: 25%;
-    
   }
 
   .card:hover {
--- frontend/src/pages/calendar_page.svelte
+++ frontend/src/pages/calendar_page.svelte
@@ -1,19 +1,17 @@
 <script>
-import MonthCalendar from '../components/month_calendar.svelte'
+  import MonthCalendar from "../components/month_calendar.svelte";
 
-const monthClick = (e) => {
-  console.log(e)
-  console.log("Uste hizo click en " + e.target.month)
-}
-
+  const monthClick = (e) => {
+    console.log(e);
+    console.log("Uste hizo click en " + e.target.month);
+  };
 </script>
-
 
 <h1 class="mt-5"><span class="months_titel"> Calendario de Ferias </span></h1>
 
 <div class="months">
-  <MonthCalendar month="Enero" onClick={monthClick}></MonthCalendar>
-  <MonthCalendar month="Febrero" onClick={monthClick}></MonthCalendar>
+  <MonthCalendar month="Enero" onClick={monthClick} />
+  <!-- <MonthCalendar month="Febrero" onClick={monthClick}></MonthCalendar>
   <MonthCalendar month="Marzo" onClick={monthClick}></MonthCalendar>
   <MonthCalendar month="Abril" onClick={monthClick}></MonthCalendar>
   <MonthCalendar month="MAyonesa" onClick={monthClick}></MonthCalendar>
@@ -23,16 +21,13 @@ const monthClick = (e) => {
   <MonthCalendar month="Septiembre" onClick={monthClick}></MonthCalendar>
   <MonthCalendar month="Obtubre" onClick={monthClick}></MonthCalendar>
   <MonthCalendar month="Noviembre" onClick={monthClick}></MonthCalendar>
-  <MonthCalendar month="Diciembre" onClick={monthClick}></MonthCalendar>
+  <MonthCalendar month="Diciembre" onClick={monthClick}></MonthCalendar> -->
 </div>
 
-
-
 <style>
-
-@font-face {
+  @font-face {
     font-family: PR Viking;
-    src: url('fonts/PR_Viking.ttf');
+    src: url("fonts/PR_Viking.ttf");
   }
 
   .months {
@@ -42,7 +37,7 @@ const monthClick = (e) => {
     justify-content: space-around;
   }
 
-  .months_titel{
+  .months_titel {
     font-family: PR Viking;
     font-size: 50pt;
     text-align: center;
