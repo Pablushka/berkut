6b517a2b7b8fedb653783c03bae819fc67001168
nati <megumi_kisukisu@hotmail.com>
2023-07-10 10:37:30 -0300

    img button add + fm_del_sur pag

---
+++ frontend/src/pages/fm_del_sur.svelte
@@ -0,0 +1,145 @@
+
+<script>
+    import fm_01 from "../../public/img/eventos/f_m_del_sur/fm_01.jpg";
+    import fm_02 from "../../public/img/eventos/f_m_del_sur/fm_02.jpg";
+    import fm_03 from "../../public/img/eventos/f_m_del_sur/fm_03.jpg";
+    import fm_04 from "../../public/img/eventos/f_m_del_sur/fm_04.jpg";
+    import fm_05 from "../../public/img/eventos/f_m_del_sur/fm_05.jpg";
+    import fm_06 from "../../public/img/eventos/f_m_del_sur/fm_06.jpg";
+    import fm_07 from "../../public/img/eventos/f_m_del_sur/fm_07.jpg";
+    import fm_08 from "../../public/img/eventos/f_m_del_sur/fm_08.jpg";
+    import fm_09 from "../../public/img/eventos/f_m_del_sur/fm_09.jpg";
+    import fm_10 from "../../public/img/eventos/f_m_del_sur/fm_10.jpg";
+  </script>
+  
+  <div class="anti-pajaro">
+    <h1 class="mt-5"><span class="gallery_text">Feria Medieval del Sur</span></h1>
+  
+    <div class="gallerys">
+      <div class="card">
+        <div>
+          <img class="gallery_photo" src={fm_01} alt="Natalia Cesario" />
+        </div>
+      </div>
+    </div>
+    
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_02} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_03} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_04} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_05} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_06} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_07} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_08} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_09} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+
+    <div class="gallerys">
+        <div class="card">
+          <div>
+            <img class="gallery_photo" src={fm_10} alt="Natalia Cesario" />
+          </div>
+        </div>
+    </div>
+  
+  </div>
+  
+  <style>
+    .gallery_photo {
+      width: 200px;
+      border-radius: 3%;
+    }
+  
+    .gallerys {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      
+    }
+  
+    .gallerys .card {
+      min-height: 300px;
+      margin: 10pt;
+      text-align: center;
+      background-color: rgba(255, 255, 255, 0);
+      border-radius: 45px;
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: row;
+      padding: 20px;
+    }
+  
+    .gallerys .card p {
+      text-align: center;
+      font-size: 1em;
+    }
+  
+    .gallery_text {
+      font-family: PR Viking;
+      font-size: 50pt;
+      text-align: center;
+    }
+  
+    .anti-pajaro {
+      width: 1500px;
+      border: solid 3px;
+      margin-top: 40px;
+      background-color: #ffffffed;
+      border-radius: 42px;
+    }
+  
+  </style>
+  
--- frontend/src/pages/gallery_page.svelte
+++ frontend/src/pages/gallery_page.svelte
@@ -2,25 +2,39 @@
 <script>
   import avellaneda_viaja_en_el_tiempo from "../../public/img/eventos/avellaneda_viaja_en_el_tiempo.jpg";
   import f_m_malvinas_argentinas from "../../public/img/eventos/f_m_malvinas_argentinas.jpg";
+  import f_m_del_sur from "../../public/img/eventos/f_m_del_sur.jpg";
+
+  import { Button } from "sveltestrap";
+  import Router from "svelte-spa-router";
+  import fm_del_sur from "./fm_del_sur.svelte";
 </script>
 
 <div class="anti-pajaro">
-  <h1 class="mt-5"><span class="member_text">Recuerdos de Eventos Pasados</span></h1>
+  <h1 class="mt-5"><span class="gallery_text">Recuerdos de Eventos Pasados</span></h1>
 
-  <div class="members">
+  <div class="gallerys">
     <div class="card">
       <div>
-        <img class="member_photo" src={avellaneda_viaja_en_el_tiempo} alt="Natalia Cesario" />
-        <p class="member_text">Avellaneda Viaja En El Tiempo, Mayo 2023</p>
+        <button class="image-button"><img class="gallery_photo" src={avellaneda_viaja_en_el_tiempo} alt="avellaneda_viaja_en_el_tiempo" /></button>
+        <p class="gallery_text">Avellaneda Viaja En El Tiempo, Mayo 2023</p>
       </div>
     </div>
   </div>
 
-  <div class="members">
+  <div class="gallerys">
     <div class="card">
       <div>
-        <img class="member_photo" src={f_m_malvinas_argentinas} alt="Natalia Cesario" />
-        <p class="member_text">Feria Medieval Malvinas Argentinas, julio 2023</p>
+        <button class="image-button"><img class="gallery_photo" src={f_m_malvinas_argentinas} alt="f_m_malvinas_argentinas" /></button>
+        <p class="gallery_text">Feria Medieval Malvinas Argentinas, Julio 2023</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="gallerys">
+    <div class="card">
+      <div>
+        <button class="image-button"><img class="gallery_photo" src={f_m_del_sur} alt="f_m_del_sur" /></button>
+        <p class="gallery_text">Feria Medieval Del Sur, Agosto 2022</p>
       </div>
     </div>
   </div>
@@ -28,19 +42,19 @@
 </div>
 
 <style>
-  .member_photo {
+  .gallery_photo {
     width: 200px;
     border-radius: 3%;
   }
 
-  .members {
+  .gallerys {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
     
   }
 
-  .members .card {
+  .gallerys .card {
     min-height: 300px;
     margin: 10pt;
     text-align: center;
@@ -49,14 +63,15 @@
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
+    padding: 20px;
   }
 
-  .members .card p {
+  .gallerys .card p {
     text-align: center;
     font-size: 1em;
   }
 
-  .member_text {
+  .gallery_text {
     font-family: PR Viking;
     font-size: 50pt;
     text-align: center;
@@ -68,6 +83,10 @@
     margin-top: 40px;
     background-color: #ffffffed;
     border-radius: 42px;
+  }
+
+  .image-button {
+    padding: 0px;
   }
 
 </style>
